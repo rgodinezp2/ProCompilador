532983cc6a33302198fc880753d93e7998000949
rgodinezp2 <99831832+rgodinezp2@users.noreply.github.com>
2023-06-16 23:41:42 -0600

    Add files via upload

---
+++ PFCompiladores/Calcparse/Cargador.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Antlr4;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Misc;
+using Antlr4.Runtime.Tree;
+
+namespace Calcparse
+{
+    public class Cargador : ExprBaseListener
+    {
+            // la pila será de tipo double
+        public Stack<double> pila { get; set; } = new Stack<double>();
+      
+        public override void ExitExpr([NotNull] ExprParser.ExprContext context)
+        {
+            base.ExitExpr(context);
+
+
+            if (context.ChildCount == 3)
+            {
+
+                // al sacar el elemento de la pila lo almacenamos en las variables.
+                double derecha = pila.Pop();
+                double izquierda = pila.Pop();
+
+
+
+                if (context.op.Type == ExprParser.SUM)
+                {
+
+                // si es SUM agrega la suma en la pila. 
+                    pila.Push(izquierda + derecha);
+
+                }
+
+                if (context.op.Type == ExprParser.RES)
+                {
+
+                    pila.Push(izquierda - derecha);
+
+
+                }
+
+                if (context.op.Type == ExprParser.MULT)
+                {
+
+                    pila.Push(izquierda * derecha);
+
+
+                }
+
+                if (context.op.Type == ExprParser.DIV)
+                {
+                    if (derecha > 0)
+                    {
+                        pila.Push(izquierda / derecha);
+
+                    }
+                    else
+                    {
+                        pila.Push(-1);
+                    }
+
+                }
+
+            }
+        }
+
+        public override void VisitTerminal([NotNull] ITerminalNode node)
+        {
+
+            base.VisitTerminal(node);
+            IToken simbolo = node.Symbol;
+            if ((simbolo.Type == ExprParser.INT) || (simbolo.Type == ExprParser.REAL))
+            {
+                //convertimos el tipo de dato
+                pila.Push(double.Parse(simbolo.Text));
+
+            }
+
+        }
+
+    }
+}
---
+++ PFCompiladores/Calcparse/Expr.g4
@@ -0,0 +1,19 @@
+﻿grammar Expr;
+prog:   (expr  NEWLINE )* ;
+expr:   expr op=(MULT|DIV) expr { Console.WriteLine("Token Encontrado:" + $op.text); } 
+    |   expr op=(SUM|RES) expr {Console.WriteLine("Token Encontrado:" + $op.text);}
+    |   INT                 {Console.WriteLine("Token Encontrado:" + $INT.text);}
+    /*agregamos la regla y lo mostramos en consola*/
+    |   REAL                {Console.WriteLine("Token Encontrado:"+ $REAL.text);}
+    ;
+
+
+MULT : '*';
+DIV : '/';
+SUM: '+';
+RES: '-';
+NEWLINE : [\r\n]+ ;
+INT     : '-'?[0-9]+ ;
+/*agregamos una regla (REAL) y agregamos el signo menos con ? para describir que es opcional, luego 
+la regla int seguido de un literal (el punto) y por ultimo otro int para las decimas*/
+REAL : '-'?INT'.'INT;
